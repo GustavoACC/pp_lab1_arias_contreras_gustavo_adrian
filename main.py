5865cac0ec764169e145768db53a867f23d36982
GustavoACC <gustavo.ariasc@hotmail.com>
2023-05-30 21:03:28 -0300

    Bonus y Extras

--- main.py
+++ main.py
@@ -35,6 +35,12 @@ def imprimir_menu():
     print("18. Mostrar jugadores que superen el porcentaje de tiros triples según valor")
     print("19. Mostrar el jugador con mayor cantidad de temporadas jugadas")
     print("20. Mostrar jugadores ordenados por posición que tengan un porcentaje de tiros de campo superior según valor")
+    print("21. Bonus: Calcular de cada jugador cuál es su posición en cada uno de los siguientes ranking - Puntos, Rebotes, Asistencias, Robos")
+    print("Opciones Extras:")
+    print("22. Mostrar la cantidad de jugadores en cada posición")
+    print("23. Mostrar la lista de jugadores ordenada por la cantidad de All-Star de forma descendente")
+    print("24. Determinar que jugador tiene las mejores estadisticas en cada valor")
+    print("25. Determinar que jugador tiene las mejores estadisticas de todos")
     print("0. Salir del programa")
 
 
@@ -424,6 +430,185 @@ def imprimir_jugadores_supera_valor_ordenados(lista_jugadores):
         lista_ordenada, 'porcentaje_tiros_de_campo', True)
 
 
+def imprimir_cantidad_jugadores_por_posicion(lista_jugadores):
+    '''
+    Imprime en consola el total de jugadores de cada posicion
+    Parametros:
+        lista_jugadores: Lista de jugadores del cual se obtendran los valores
+    No retorno valores
+    '''
+    posiciones = {}
+    for jugador in lista_jugadores:
+        if jugador['posicion'] in posiciones:
+            posiciones[jugador['posicion']] += 1
+        else:
+            posiciones[jugador['posicion']] = 1
+    posiciones_keys = posiciones.keys()
+    for key in posiciones_keys:
+        imprimir_valor_referencia(key, posiciones[key])
+
+
+def imprimir_mejor_en_estadistica(lista_jugadores, key: str):
+    '''
+    Imprime el mejor jugador por la estadistica deseada y su valor
+    Parametros:
+        lista_jugadores: Lista de jugadores del cual se obtendran los valores
+        key: Clave de la estadisctica por la cual se quiere obtener el mayor
+    No retorno valores
+    '''
+    mensaje = 'Mayor cantidad de {0}'.format(key)
+    lista_ordenada = quick_sort_jugador_estadistica(
+        lista_jugadores, key, False)
+    imprimir_valor_referencia(
+        mensaje, lista_ordenada[0]['nombre'], lista_ordenada[0]['estadisticas'][key])
+
+
+def imprimir_mejor_por_estadistica(lista_jugadores):
+    '''
+    Imprime un lista de los mejores jugadores por estadistica disponible
+    Parametros:
+        lista_jugadores: Lista de jugadores del cual se obtendran los valores
+    No retorno valores
+    '''
+    # Obtengo el lista de las estadisticas disponibles
+    estadisticas_keys = lista_jugadores[0]['estadisticas'].keys()
+    for key in estadisticas_keys:
+        imprimir_mejor_en_estadistica(lista_jugadores, key)
+
+
+def imprimir_jugadores_por_cantidad_allstar(lista_jugadores):
+    '''
+    Imprime de forma descendiente los jugadores por cantidad de veces del logro de All-Star
+    Parametros:
+        lista_jugadores: Listado de jugadores del cual se obtendran los valores
+    No retorna valores
+    '''
+    aux_lista_jugadores = []
+    for jugador in lista_jugadores:
+        # Agrego una flag en caso de no tener el logro
+        encontrado = False
+        for logro in jugador['logros']:
+            if re.search(r'All-Star', logro):
+                encontrado = True
+                aux_allstar = logro.split(' ')
+                aux_diccionario = {
+                    'nombre': jugador['nombre'], 'allStar': logro, 'cantidad_allStar': int(aux_allstar[0])}
+                aux_lista_jugadores.append(aux_diccionario)
+
+        # En caso de no tener el logro se lo agrega como 0 para que no se pierda la cantidad total de jugadores
+        if not encontrado:
+            aux_diccionario = {
+                'nombre': jugador['nombre'], 'allStar': '0 veces All-Star', 'cantidad_allStar': int(0)}
+            aux_lista_jugadores.append(aux_diccionario)
+
+    lista_ordenada = quick_sort_jugador_no_diccionario(
+        aux_lista_jugadores, 'cantidad_allStar', False)
+    for jugador in lista_ordenada:
+        imprimir_valor_referencia(jugador['nombre'], jugador['allStar'])
+
+
+def guardar_posicion_estadistica(lista_jugadores, lista_ordenada, key_estadisticas, key: str):
+    '''
+    Guarda valores en la lista referenciada en base a una lista previamente ordenada y la clave que se utilizo para ordenarla
+    Parametros:
+        lista_jugadores: Lista de jugadores donde se guardara el resultado final
+        lista_ordenada: Lista de jugadores ordenada para obtener la posicion
+        key_estadisticas: Clave donde se desea guardar dentro de la lista_jugadores el resultado
+        key: Clave de la estadistica precisa a guardar su posicion de la lista ordenada
+    No retorna valores
+    '''
+    for index, jugador_ordenado in enumerate(lista_ordenada):
+        for jugador in lista_jugadores:
+            if (jugador['nombre'] == jugador_ordenado['nombre']):
+                jugador[key_estadisticas][key] = index + 1
+
+
+def guardar_lista_posicion_estadistica_jugador(lista_jugadores, lista_diccionarios, key_estadisticas, lista_keys):
+    '''
+    Guarda valores dentro de a lista referenciada, ordenando y guardando la posicion en base a la lista de keys solicitada
+    Parametros:
+        lista_jugadores: Lista de jugadores donde se obtendran los valores
+        lista_diccionarios: Lista donde se guardara por referencia el resultado final
+        key_estadisticas: Clave que se utilizara para obtener las estadisticas de la lista de diccionarios
+        lista_keys: Lista de Claves que se desean obtener las posiciones
+    No retorna valores
+    '''
+    for jugador in lista_jugadores:
+        aux_diccionario_inicial = {
+            'nombre': jugador['nombre'], key_estadisticas: {}}
+        lista_diccionarios.append(aux_diccionario_inicial)
+
+    for key in lista_keys:
+        aux_lista_ordenada = quick_sort_jugador_estadistica(
+            lista_jugadores, key, False)
+        guardar_posicion_estadistica(
+            lista_diccionarios, aux_lista_ordenada, key_estadisticas, key)
+
+
+def imprimir_mejor_jugador_total_estadisticas(lista_jugadores):
+    '''
+    Imprime en pantalla al jugador con mejores estadisticas dependiendo de su posicion general en todas las mismas
+    Parametros:
+        lista_jugadores: Lista de jugadores del cual se obtendran los valores
+    No retorna valores
+    '''
+    aux_lista_diccionarios = []
+    aux_keys = lista_jugadores[0]['estadisticas'].keys()
+    guardar_lista_posicion_estadistica_jugador(
+        lista_jugadores, aux_lista_diccionarios, 'posicion_estadisticas', aux_keys)
+
+    aux_lista_totales = []
+    for jugador in aux_lista_diccionarios:
+        aux_total = 0
+        for key in aux_keys:
+            aux_total += jugador['posicion_estadisticas'][key]
+        aux_diccionario_totales = {
+            'nombre': jugador['nombre'], 'total_estadisticas': aux_total}
+        aux_lista_totales.append(aux_diccionario_totales)
+    total_ordenados = quick_sort_jugador_no_diccionario(
+        aux_lista_totales, 'total_estadisticas')
+    imprimir_valor_referencia(
+        'El jugador con mayores estadisticas', total_ordenados[0]['nombre'])
+
+
+def obtener_lista_posiciones_estadisticas_textual(jugador, lista_estadisticas):
+    '''
+    Devuelve una lista de strings con los valores dentro de las estadisticas solicitadas
+    Parametros:
+        jugador: Jugador del cual se obtienen los valores
+        lista_estadisticas: Lista de claves con las estadisticas a guardar
+    Retorna una lista de strings
+    '''
+    listado_resultados = []
+    for estadistica in lista_estadisticas:
+        listado_resultados.append(str(jugador['estadisticas'][estadistica]))
+    return listado_resultados
+
+
+def generar_csv_posiciones_estadisticas(lista_jugadores, lista_estadisticas):
+    '''
+    Genera un archivo Csv con la posicion en la que se registro de manera descendente en base a las claves de las estadisticas solicitadas
+    Parametros:
+        lista_jugadores: Lista de los jugadores de los cuales se obtienen los valores
+        lista_estadisticas: Lista de claves que se desea obtener la posicion una vez ordenadas de forma Descendente
+    No retorna valores
+    '''
+    aux_lista_diccionarios = []
+    guardar_lista_posicion_estadistica_jugador(
+        lista_jugadores, aux_lista_diccionarios, 'estadisticas', lista_estadisticas)
+    aux_columnas = 'jugadores,{0}'.format(','.join(lista_estadisticas))
+    aux_jugadores_columnas = ''
+    for jugador in aux_lista_diccionarios:
+        aux_estadisticas = obtener_lista_posiciones_estadisticas_textual(
+            jugador, lista_estadisticas)
+        print(aux_estadisticas)
+        aux_jugador = '\n{0},{1}'.format(
+            jugador['nombre'], ','.join(aux_estadisticas))
+        aux_jugadores_columnas += aux_jugador
+    archivo = '{0}{1}'.format(aux_columnas, aux_jugadores_columnas)
+    generar_csv(archivo, 'posicion_estadisticas')
+
+
 # Guardo una copia de los valores del archivo
 json_copy = obtener_valores_json(path_json_file)
 
@@ -486,6 +671,19 @@ while True:
                 json_copy['jugadores'], 'temporadas')
         case 20:
             imprimir_jugadores_supera_valor_ordenados(json_copy['jugadores'])
+        case 21:
+            lista_estadisticas = [
+                'puntos_totales', 'rebotes_totales', 'asistencias_totales', 'robos_totales']
+            generar_csv_posiciones_estadisticas(
+                json_copy['jugadores'], lista_estadisticas)
+        case 22:
+            imprimir_cantidad_jugadores_por_posicion(json_copy['jugadores'])
+        case 23:
+            imprimir_jugadores_por_cantidad_allstar(json_copy['jugadores'])
+        case 24:
+            imprimir_mejor_por_estadistica(json_copy['jugadores'])
+        case 25:
+            imprimir_mejor_jugador_total_estadisticas(json_copy['jugadores'])
         case 0:
             break
         case _:
